ad7976369a19d424a1d481c188f14721f8abce57
Nagraj94 <104769494+Nagraj94@users.noreply.github.com>
2022-07-28 11:40:23 +0530

    Update LinkedList.java

--- src/com/brigdelabz/LinkedList.java
+++ src/com/brigdelabz/LinkedList.java
@@ -4,7 +4,7 @@ public class LinkedList {
     Node head;
     Node tail;
 
-    public void push(int data){
+    public void pushLL(int data){
         Node newNode = new Node(data);
         if(head == null) {
             head = newNode;
